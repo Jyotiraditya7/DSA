1075325a4321d1dfb9b5fba6f7bc050c37df5388
Jyotiraditya7 <142438419+Jyotiraditya7@users.noreply.github.com>
2024-01-20 16:29:41 +0530

    Time: 110 ms (39.88%) | Memory: 61.2 MB (8.49%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,20 @@
+/* 
+1)min heap bana ke topmost(min) element delete kiya
+2)heap is of k size, so if 1st largest so only 1 element(largest) will be in heap,each time smaller being rejected
+3) at end just pop heap
+*/
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue <int, vector<int>, greater<int>>min_heap;
+        for(int i =0;i<nums.size();i++){
+            min_heap.push(nums[i]);
+            if(min_heap.size()>k){
+                min_heap.pop();
+            }
+        }
+        return min_heap.top();
+    }
+};
