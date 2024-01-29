eca876925349fce944b332a97999754ed0ed6f91
Jyotiraditya7 <142438419+Jyotiraditya7@users.noreply.github.com>
2024-01-29 11:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (7.61%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,3 +1,16 @@
+/*
+If using 2 queues:
+1) push to q2
+2) transfer all els from q1 to q2(topwise)
+3) now swap q2 with q1
+basically idea is to implement lifo(stack) through a queue, therefore pushed element goes to front of queue(last in) and will be removed first(first out)
+
+If using 1 queue:
+1)push to q. now el is at rear, we have to somehow get el to front of q
+2) trasnfer all els from i =0 to i<q.size()-1 that is if 3 els then 2 will be pushed to q and popped>> push the (front) of the queue,pop and then increment i
+3) now el is at front at rest els are at rear
+*/
+
 class MyStack {
 public:
     queue<int>q;
