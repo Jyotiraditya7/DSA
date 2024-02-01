5740d7d16af510bf173a89068c0166b39101373c
Jyotiraditya7 <142438419+Jyotiraditya7@users.noreply.github.com>
2024-02-01 12:06:12 +0530

    Time: 2 ms (90.80%) | Memory: 12 MB (16.06%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,3 +1,20 @@
+/*
+1) iterate thorugh nums2 and apply the following logic:
+if stack is empty then push nums2's el and its greater is nothing(or = -1)
+if stack is not empty and nums2 el > stack's top el ,pop the stack till nums el encounters
+an element in stack which is greater 
+if stack top is greater then push nums2 el in stack
+consider this: we are traversing from left to right, so when nums el > top and we pop, the 
+nums2 el appears on the right side of popped el in array and hence it is just greater
+element of top. so in map go to the key(=top element) and makes its value(greater element) as nums2 el
+
+2) create an ans vector with nums1 size, since we only have to return those many elements 
+which are just greater than each nums els.
+3) run loop on nums1 and check if nums1 el exists in map, store its value in ans.(this will
+only happen when in map its value(greater element) is updated via the while loop) otherwise
+if an els does not exist in map then mp.find() = end and ans does not update its value(=defualt -1)
+*/
+
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
