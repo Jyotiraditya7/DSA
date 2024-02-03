3571ffd12e62736a795c955168b6380d3372ac61
Jyotiraditya7 <142438419+Jyotiraditya7@users.noreply.github.com>
2024-02-03 11:26:42 +0530

    Time: 3 ms (74.97%) | Memory: 12.4 MB (19.63%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        vector<int> ans = prices;
+        /*vector<int> ans = prices;
         stack<int> s;
         
         for(int i=prices.size() - 1; i>=0; i--) {
@@ -10,6 +10,18 @@ public:
             s.push(prices[i]);
         }
         
+        return ans;*/
+        stack<int>st;
+        vector<int>ans=prices;
+        for(int i = prices.size()-1;i>=0;i--){
+            while(!st.empty() && st.top()>prices[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                ans[i]= ans[i]-st.top();
+            }
+            st.push(prices[i]);
+        }
         return ans;
     }
 };
