a7589f92bd9b1f81a3051302f9403542519f5745
Jyotiraditya7 <142438419+Jyotiraditya7@users.noreply.github.com>
2024-02-27 22:28:47 +0530

    Time: 11 ms (96.50%) | Memory: 21 MB (33.71%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,44 @@
+/*
+1)O(1) time and O(2N) space approach:
+create single stack and store pair of el along with curr min el till the index
+for pop return top.first
+for push push the el+min(el,min_before)
+for min return top.second
+2) O(1) time and O(N) space approach:
+*/
+
+class MinStack {
+  stack < pair < int, int >> st;
+
+  public:
+    void push(int x) {
+      int min;
+      if (st.empty()) {
+        min = x;
+      } else {
+        min = std::min(st.top().second, x);
+      }
+      st.push({x,min});
+    }
+
+  void pop() {
+    st.pop();
+  }
+
+  int top() {
+    return st.top().first;
+  }
+
+  int getMin() {
+    return st.top().second;
+  }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
