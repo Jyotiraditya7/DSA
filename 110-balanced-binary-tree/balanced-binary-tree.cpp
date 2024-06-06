57ae7cace838473c26abe68720dadecc573dab4f
Jyotiraditya7 <142438419+Jyotiraditya7@users.noreply.github.com>
2024-06-06 19:51:25 +0530

    Time: 4 ms (93.46%) | Memory: 21.4 MB (95.71%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //balanced binary tree: for every node abs(lh-rh)<=1//
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
@@ -20,9 +21,9 @@ public:
         }
         int lh = bal(node->left);
         int rh = bal(node->right);
-        if(lh==-1 || rh ==-1)
-            return -1;
-        if(abs(lh-rh)>1)
+        if(lh==-1 || rh ==-1)  // if(lh || rh ==-1) this is not correct checking statement bcz it will evaluate to
+            return -1; //           (lh||rh) is this == true(anything apart from 0 is true and 0 is false so here -1 means true)
+        if(abs(lh-rh)>1) //         so true||anything = true and true ==-1 yes then it return -1 which is not correct
             return -1;
         return(1+max(lh,rh));
     }
