c5c4fecea18ac9545b64801a4f0659e18358e5df
Jyotiraditya7 <142438419+Jyotiraditya7@users.noreply.github.com>
2024-06-19 19:51:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (48.72%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,8 +3,8 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string ans="";
         int n = strs.size();
-        sort(strs.begin(),strs.end());
-        for(int i =0;i<min(strs[0].size(),strs[n-1].size());i++){
+        sort(strs.begin(),strs.end()); // this will sort lexicographically and we only need
+        for(int i =0;i<min(strs[0].size(),strs[n-1].size());i++){ // to compare first and last word
             if(strs[0][i]!=strs[n-1][i]){
                 return ans;
             }
