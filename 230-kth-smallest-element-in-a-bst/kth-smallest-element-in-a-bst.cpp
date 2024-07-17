39bd25dc834df037c323ce4ee1edfe84f26b6f02
Jyotiraditya7 <142438419+Jyotiraditya7@users.noreply.github.com>
2024-07-17 19:36:37 +0530

    Time: 11 ms (73.50%) | Memory: 22.8 MB (23.75%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    void fn(TreeNode* root, int k,int &count,int &ans){
+   /*void fn(TreeNode* root, int k,int &count,int &ans){
         if(root==NULL)
             return;
         fn(root->left,k,count,ans);
@@ -26,6 +26,46 @@ public:
         int count=0;
         int ans;
         fn(root,k,count,ans);
+        return ans;
+    }*/
+    // we can do morris traversal because recursion takes recursion stack space O(N) whereas morris is O(1) space
+    
+    void morris(TreeNode* root,int k, int &ans){
+        TreeNode* cur=root;
+        int count=0;
+        while(cur!=NULL){
+            if(cur->left ==NULL){
+                count++; // in this case we usually insert into inorder array
+                if(count==k){
+                    ans= cur->val;
+
+                }
+                cur=cur->right;
+            }
+            else{
+                TreeNode* prev = cur->left;
+                while(prev->right != NULL && prev->right !=cur){ // imp to note the && condition too
+                    prev = prev->right;
+                }
+                if(prev->right ==NULL){
+                    prev->right = cur;
+                    cur=cur->left;
+                }
+                else{
+                    prev->right = NULL;
+                    count++; // at this step we usually insert cur into inorder vector
+                    if(count==k){
+                        ans = cur->val;
+                    }
+                    cur = cur->right; // as left processed, node processed after returning from left subtree, now process right
+                }
+
+            }
+        }
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        int ans;
+        morris(root,k,ans);
         return ans;
     }
 };
